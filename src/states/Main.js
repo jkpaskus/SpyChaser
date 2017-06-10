feaa18c047326f586fcd32714ab2a9937b110272
BobbyB <bobbyb@bobonthenet.com>
2017-06-09 23:05:49 -0400

    Removed Service Worker Stuff

--- src/states/Main.js
+++ src/states/Main.js
@@ -10,12 +10,14 @@ class Main extends Phaser.State {
 		//Set the games background colour
 		this.game.stage.backgroundColor = '#cecece';
 
+		var phaser = this.game.add.sprite(80, 0, 'phaser');
+
 		//Example of including an object
 		//let exampleObject = new ExampleObject(this.game);
 	}
 
 	update() {
-		
+
 	}
 
 }
--- src/states/Preload.js
+++ src/states/Preload.js
@@ -5,6 +5,7 @@ class Preload extends Phaser.State {
 		//this.game.load.image('myImage', 'assets/my-image.png');
 		//this.game.load.audio('myAudio', 'assets/my-audio.wav');
 		//this.game.load.atlas('myAtlas', 'assets/my-atlas.png', 'assets/my-atlas.json');
+		this.game.load.image('phaser', 'assets/phaser.png');
 	}
 
 	create() {
